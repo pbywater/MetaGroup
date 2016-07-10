5efe4af06924a235028397ea2512b084816010c6
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-10 22:33:07 +0100

    Character countdown added
    Counts down how many characters user has left in Comments box

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+$(document).ready(function() {
+
+//Allows only numbers
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : event.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
         return false;
     return true;
 }
+   
+//Text character countdown
+
+var text_max = 1000;
+$('#count_message').html(text_max + ' remaining');
+$('#text').keyup(function() {
+	var text_length = $('#text').val().length;
+	var text_remaining = text_max - text_length;
+	$('#count_message').html(text_remaining + ' remaining');
+});
+        
+});
