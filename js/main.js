67a610f04ee98eb81824fa132ab3b9efe65c2b54
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-07-14 16:17:21 +0100

    Addition of back to top button on About page
    Need to enable button. Not working at the moment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // FUNCTIONS TO BE RUN AFTER PAGE HAS LOADED
 $(document).ready(function() {   
 
-
-//Text character countdown
+//TEXT CHARACTER COUNT
 
 var text_max = 1000;
 $('#count_message').html(text_max + ' characters remaining');
@@ -11,10 +10,33 @@ $('#text').keyup(function() {
     var text_remaining = text_max - text_length;
     $('#count_message').html(text_remaining + ' characters remaining');
 });
-        
+
+//END OF CHARACTER COUNT
+
+// BACK TO TOP BUTTON
+
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 100,
+  //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  offset_opacity = 2000, //1200px??
+  //duration of the top scrolling animation (in ms)
+  scroll_top_duration = 700,
+  //grab the "back to top" link
+  $back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+  ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  if( $(this).scrollTop() > offset_opacity ) { 
+    $back_to_top.addClass('cd-fade-out');
+  }
 });
 
 
+// END OF BACK TO TOP BUTTON
+        
+});
+// END OF FUNCTIONS TO BE RUN AFTER PAGE LOADS
 
 //FUNCTIONS TO BE RUN BEFORE PAGE HAS LOADED
 
